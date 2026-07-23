c6e3a8e2b750ea76d3f48c2bd37f98defd1a52d5
Jan Vos <jjcvos@hotmail.nl>
2026-07-23 14:35:32 +0200

    double chekc

--- src/dmdreader.cpp
+++ src/dmdreader.cpp
@@ -896,6 +896,7 @@ void dmd_dma_handler() {
   switch_buffers();
 
   if (frame_crc != crc_previous_frame) {
+    Serial.printf("frame received!\n");
     crc_previous_frame = frame_crc;
     frame_received = true;
   }
