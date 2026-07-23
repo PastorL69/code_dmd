503f3bf835d5fa0969eeffc67901b69b428fbf7c
Jan Vos <jjcvos@hotmail.nl>
2026-07-23 12:27:20 +0200

    check for backwards compatibility crc calc

--- src/dmdreader.cpp
+++ src/dmdreader.cpp
@@ -633,6 +633,9 @@ void dmd_dma_handler() {
   dma_crc = dma_hw->sniff_data;
   dmd_set_and_enable_new_dma_target();
 
+  frame_crc =
+    crc32(0, currentPlaneBuffer, loopback ? source_bytes : target_bytes);
+
   if (dmd_type == DMD_DE_X16_V2) {
     // Due to the complexity of x16 v2, we use this way to re-sync
     // if the signals are noisy, or whatever else could happen.
@@ -894,8 +897,8 @@ void dmd_dma_handler() {
 
   uint32_t normal_crc1 = micros();
 
-  frame_crc =
-      crc32(0, current_framebuf, loopback ? source_bytes : target_bytes);
+  // frame_crc =
+  //     crc32(0, current_framebuf, loopback ? source_bytes : target_bytes);
 
   uint32_t normal_crc2 = micros();
 
@@ -1505,9 +1508,13 @@ bool dmdreader_init(bool return_on_no_detection) {
                           pio_get_dreq(dmd_pio, dmd_sm, false));
 
   // Make use of the built-in CRC32 calculator for duplicate frames.
+  // CRC-32 standard configurations:
+  // Bit-reverse and invert output to match standard bit-reflected CRC32
   channel_config_set_sniff_enable(&dmd_dma_channel_cfg, true);
-  dma_hw->sniff_data = 0xFFFFFFFF;
   dma_sniffer_enable(dmd_dma_channel, DMA_SNIFF_CTRL_CALC_VALUE_CRC32, true);
+  dma_hw->sniff_ctrl |= DMA_SNIFF_CTRL_OUT_INV_BITS; // Invert output bits
+  dma_hw->sniff_ctrl |= DMA_SNIFF_CTRL_OUT_REV_BITS; // Reverse output bit order
+  dma_hw->sniff_data = 0xFFFFFFFF;
 
   // Configure the DMA channel. As soon as the PIO pushed a specified number
   // of words to its RX FIFO, the DMA transfer will be triggered. The amount
