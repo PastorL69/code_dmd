e8b38de846bdd4a295692a6205563fa7d29b9fb2
Jan Vos <jjcvos@hotmail.nl>
2026-07-23 13:14:47 +0200

    false?

--- src/dmdreader.cpp
+++ src/dmdreader.cpp
@@ -1497,7 +1497,7 @@ bool dmdreader_init(bool return_on_no_detection) {
 
   // Make use of the built-in CRC32 sniffer for duplicate frames.
   //channel_config_set_sniff_enable(&dmd_dma_channel_cfg, true);
-  dma_sniffer_enable(dmd_dma_channel, DMA_SNIFF_CTRL_CALC_VALUE_SUM, true);
+  dma_sniffer_enable(dmd_dma_channel, DMA_SNIFF_CTRL_CALC_VALUE_CRC32R, false);
   dma_hw->sniff_data = 0;
 
   // Configure the DMA channel. As soon as the PIO pushed a specified number
