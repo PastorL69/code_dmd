d4c34d9e093407780e4b4f5ae19c222014e4cef6
Jan Vos <jjcvos@hotmail.nl>
2026-07-23 12:50:11 +0200

    test without serial

--- src/dmdreader.cpp
+++ src/dmdreader.cpp
@@ -899,8 +899,11 @@ void dmd_dma_handler() {
 
   switch_buffers();
 
+  // frame_crc =
+  //   crc32(0, current_framebuf, loopback ? source_bytes : target_bytes);
+
   if (frame_crc != crc_previous_frame) {
-    Serial.printf("crc 32 with dma: 0x%08X\n", frame_crc);
+    //Serial.printf("crc 32 with dma: 0x%08X\n", frame_crc);
     crc_previous_frame = frame_crc;
     frame_received = true;
   }
