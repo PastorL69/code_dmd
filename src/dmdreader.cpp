dff6cbce6e55aae1a903aea708d40977006ccc42
Jan Vos <jjcvos@hotmail.nl>
2026-07-27 16:45:01 +0200

    debugger

--- src/dmdreader.cpp
+++ src/dmdreader.cpp
@@ -908,6 +908,7 @@ void dmd_dma_handler() {
 
   if (!std::is_permutation(current_crc, current_crc + crc_bytes, prev_crc)) {
     frame_received = true;
+    Serial.printf("received frame: %08X\n", frame_crc);
   }
 
   memcpy(prev_crc, current_crc, crc_bytes);
