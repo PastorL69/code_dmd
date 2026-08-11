6949a66b36822c64c23efc7f56bfcc1039ac3c2b
Jan Vos <jjcvos@hotmail.nl>
2026-08-11 14:48:36 +0200

    more accurate calc

--- src/dmdreader.cpp
+++ src/dmdreader.cpp
@@ -915,10 +915,10 @@ void dmd_dma_handler() {
   switch_buffers();
 
   if (frame_crc != crc_previous_frame) {
-    Serial.printf("CRC VAL = %d\n", frame_crc);
     crc_previous_frame = frame_crc;
     frame_received = true;
     uint32_t nower2 = micros();
+    Serial.printf("CRC VAL = %d\n", frame_crc);
     Serial.printf("time diff: %d\n", nower2-nower1);
   }
 }
