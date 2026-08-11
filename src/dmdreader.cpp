f6102bd4de70578135343057b157406e499a34e7
Jan Vos <jjcvos@hotmail.nl>
2026-08-11 15:10:48 +0200

    test this mehtod

--- src/dmdreader.cpp
+++ src/dmdreader.cpp
@@ -905,22 +905,22 @@ void dmd_dma_handler() {
   memcpy(current_framebuf, processingbuf,
          loopback ? source_bytes : target_bytes);
 
-  frame_crc =
-      crc32(0, current_framebuf, loopback ? source_bytes : target_bytes);
-  // dmd_set_and_enable_new_dma_sniffer();
+  // frame_crc =
+  //     crc32(0, current_framebuf, loopback ? source_bytes : target_bytes);
+  dmd_set_and_enable_new_dma_sniffer();
 
-  // frame_crc = dma_hw->sniff_data;
-  // dma_hw->sniff_data = 0xFFFFFFFF;  // always clean after sniffing.
+  frame_crc = dma_hw->sniff_data;
+  dma_hw->sniff_data = 0xFFFFFFFF;  // always clean after sniffing.
 
   switch_buffers();
 
   if (frame_crc != crc_previous_frame) {
     crc_previous_frame = frame_crc;
     frame_received = true;
-    uint32_t nower2 = micros();
-    Serial.printf("CRC VAL = %d\n", frame_crc);
-    Serial.printf("time diff: %d\n", nower2-nower1);
   }
+  uint32_t nower2 = micros();
+  Serial.printf("CRC VAL = %d\n", frame_crc);
+  Serial.printf("time diff: %d\n", nower2 - nower1);
 }
 
 void dmdreader_error_blink(bool no_error) {
