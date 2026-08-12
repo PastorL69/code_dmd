bfe288aba791d76c2c440036f8464bbdc8e0cff6
Jan Vos <jjcvos@hotmail.nl>
2026-08-12 21:34:17 +0200

    use the already available variable instead for offset_x16.

--- src/dmdreader.cpp
+++ src/dmdreader.cpp
@@ -807,7 +807,7 @@ void dmd_dma_handler() {
     // merge the rows and convert from 4bpp to 2bpp with a LUT
     uint32_t *dst, *src1, *src2;
     dst = framebuf + 64; // start in the middle of 128x32 frame
-    src1 = framebuf + 511; // everything is stored from here onwards
+    src1 = framebuf + offset_x16; // everything is stored from here onwards
     src2 = src1 + source_dwordsperline;
 
     if (dmd_type == DMD_DE_X16_V1) {
